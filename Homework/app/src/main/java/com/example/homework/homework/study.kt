8631f4f55e0d524204fa023cb3436e4afd9c77d4
halil-ibrhm-ky <119860358+halil-ibrhm-ky@users.noreply.github.com>
2023-03-21 15:21:55 +0300

    About_kotlin

---
+++ Homework/app/src/main/java/com/example/homework/homework/study.kt
@@ -0,0 +1,107 @@
+package com.example.homework.homework
+
+fun main() {
+
+    val city="Sanlıurfa"
+    val country="Turkey"
+    val phoneNo="0512236762"
+    val postCode=63
+    val email ="kayaibrhim@gmail.com"
+    val job = "Engineer"
+    val amountStock=1500  // stok miktarı
+    val costumerName="Mustafa"
+    val balance=3680.2582
+    val birthday="19.11.2002"
+    val salary=25000
+    val maritalStatus="single"  // medeni durumu
+    val productReview="Perfect"
+    val paymentDate="20.07.2024"
+    val payment="cash"   // ödeme_nakit
+    val amountOrder=200
+    val modelCar="Mercedes"
+    val bookName="Ucurtma Avcisi"
+    val releaseDate="29.05.2003"   // yayınlanma tarihi
+    val amountDiscount=29.90       // indirim miktarı
+    val roomNumber=5
+    val latitude = "90 C "         // enlem
+    val longitude ="90 C"          // boylam
+    val productName="TV"
+    val foodPrice=78.85
+    val brand="Samsung"            // marka
+    val musicName="Phantogram-Black Out Days"
+    val videoTime=3.50
+    val productPoint=7.8
+    val pictureName="Natural Landscapes"   // doğal manzaralar
+    val fileFormat="txt"
+    val colour="Blue"
+    val colourCode="12353"
+    val phoneModel="Xiaomi"
+    val screenSize=6.3
+    val weight =1.78
+    val nationalDay="21.04.2023"
+    val holiday="04.07-01.09"
+    val reservationDate="25.03.2023"
+    val streetName="Direkli"
+    val busLine=63
+    val remainingMinute=200.35
+    val trackingCode=856430             //takip kodu
+    val couponDuration="20.03-20.07"    //kupon süresi
+    val couponCode="HelloWorld"
+    val billingAddress="Bandırma/Balıkesir"
+
+println("")
+println("****************************************************")
+println("")
+
+println("city: $city")
+println("Country: $country")
+println("Phone no: $phoneNo")
+println("Post Code: $postCode")
+println("Email: $email")
+println("Job: $job")
+println("Amount of Stock: $amountStock")
+println("Name Of costumer: $costumerName")
+println("Balance: $balance")
+println("Birthday: $birthday")
+println("Salary: $salary")
+println("Marital Status: $maritalStatus")
+println("Review of product: $productReview")
+println("Payment date: $paymentDate")
+println("Payment type: $payment")
+println("Amount of order: $amountOrder")
+println("Model of car: $modelCar")
+println("Book name: $bookName")
+println("Day of release: $releaseDate")
+println("Amount of discount: $amountDiscount")
+println("Number of room: $roomNumber")
+println("Latitude: $latitude")
+println("Longitude: $longitude")
+println("Name of product: $productName")
+println("price of food: $foodPrice")
+println("Brand of product: $brand")
+println("Name of music: $musicName")
+println("Time of video: $videoTime")
+println("Point of product: $productPoint")
+println("Name of picture: $pictureName")
+println("Format of file: $fileFormat")
+println("Colour: $colour")
+println("Colour code: $colourCode")
+println("Model of phone: $phoneModel")
+println("Size of screen: $screenSize")
+println("Weight: $weight")
+println("Day of national: $nationalDay")
+println("Holiday: $holiday")
+println("Date of reservation: $reservationDate")
+println("Name of street: $streetName")
+println("Bus line: $busLine")
+println("Minute of date: $remainingMinute")
+println("Tracking code: $trackingCode")
+println("Duration of code: $couponDuration")
+println("Coupon code: $couponCode")
+println("Billing of address: $billingAddress")
+
+println("")
+println("****************************************************")
+println("")
+
+}
